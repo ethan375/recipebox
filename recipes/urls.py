7c56f50de63132c9bb903f8fd7767e885f1475dd
ethan375 <ethanebel1@gmail.com>
2019-11-19 23:49:59 -0600

    implemented authentication

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,9 +5,21 @@ from . import views
 app_name = 'recipes'
 urlpatterns = [
     path('', views.home),
+
     path('recipes', views.recipes, name='index'),
-    path('recipe/<int:id>', views.recipe, name='recipe_detail'),
-    path('author/<int:id>', views.authors, name='author_detail'),
+
+    path('recipe/<int:id>', views.recipe, 
+    name='recipe_detail'),
+    
     path('recipe/new', views.new_recipe, name='new_recipe'),
-    path('author/new', views.new_author, name="new_author")
+
+    path('author/new', views.new_author, name="new_author"),
+
+    path('author/<int:id>', views.authors, name='author_detail'),
+
+    path('register', views.new_author, name='new_author'),
+
+    path('login', views.login_user, name='login_user'),
+
+    path('logout', views.logout_user, name='logout_user')
 ]
